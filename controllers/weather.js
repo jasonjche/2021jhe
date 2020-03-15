7b232af30b64707eaa91e083afa21141e561ecab
Jason He <jasonhe.md@gmail.com>
2020-03-15 16:40:28 -0400

    Co-authored-by: Alex L <alexlin247@gmail.com>

--- controllers/weather.js
+++ controllers/weather.js
@@ -1,5 +1,6 @@
 var https = require('https');
 var fs = require('fs');
+var hbs = require('hbs');
 
 module.exports.run_setup = function(app) {
 app.get('/weather', (req, res) => {
@@ -15,14 +16,17 @@ var options = {
 app.get('/getweather', (req, res, next) => {
         var url = 'https://api.weather.gov/points/' + req.query.lat + ',' + req.query.long;
         https.get(url, options, response => {
-            if (response.statusCode == 404)
-                res.send('oof');
-                //res.render('partials/template', {name : 'Weather Forecast', content : `URL: ${url} cannot be found.`});
-            if (response.statusCode == 301)
+            if (response.statusCode == 404) {
+                res.render('partials/template', {name : 'Weather Forecast', content : 'There is no forecast available at this location'});
+                return;
+            }   
+            if (response.statusCode == 301) {
                 res.render('partials/template', {name : 'Weather Forecast', content : 'The longitude or latitude coordinates are too precise. Please limit to 4 decimal places.'});
-            var data = '';
+                return;
+            }
+                var data = '';
             response.on('data', function(d) {
-                console.log(d.toString());
+                //console.log(d.toString());
                 data += d;
             });
             response.on('end', () => {
@@ -34,30 +38,31 @@ app.get('/getweather', (req, res, next) => {
         });
     },
     (req, res) => {
-        if (res.locals.obj.properties.forecast === null)
-            res.render('partials/template', {name : 'Weather Forecast', content : 'There is no forecast available at this location'});
-        https.get(res.locals.obj.properties.forecast, options, response => {
-            var data = '';
-            response.on('data', d => {
-                console.log(d.toString());
-                data += d;
+            https.get(res.locals.obj.properties.forecast, options, response => {
+                var data = '';
+                response.on('data', d => {
+                    //console.log(d.toString());
+                    data += d;
+                });
+                response.on('end', () => {
+                    res.locals.forecastObj = JSON.parse(data);
+                    var detailedForecasts = [];
+                    for (var forecast of res.locals.forecastObj.properties.periods) {
+                        detailedForecasts.push(forecast.name + ": " + forecast.detailedForecast);
+                    }
+                    //console.log(res.locals.forecastObj.properties.periods.detailedForecast);
+                    var render_dict = {
+                        state: res.locals.obj.properties.relativeLocation.properties.state,
+                        city: res.locals.obj.properties.relativeLocation.properties.city,
+                        forecast: detailedForecasts
+                    };
+                    //console.log(render_dict);
+
+                    var content = hbs.handlebars.compile(
+                        fs.readFileSync(__dirname + '/../views/weatherResults.hbs', 'utf-8'));
+                    res.render('partials/template', {name : 'Weather Forecast', content : content(render_dict)});
+                });
             });
-            response.on('end', () => {
-                res.locals.forecastObj = JSON.parse(data);
-                var detailedForecasts = [];
-                for (var forecast of res.locals.forecastObj.properties.periods) {
-                    detailedForecasts.push(forecast.name + ": " + forecast.detailedForecast);
-                }
-                console.log(res.locals.forecastObj.properties.periods.detailedForecast);
-                var render_dict = {
-                    state: res.locals.obj.properties.relativeLocation.properties.state,
-                    city: res.locals.obj.properties.relativeLocation.properties.city,
-                    forecast: detailedForecasts
-                };
-                res.render('partials/template', {name : 'Weather Forecast', content : fs.readFileSync(__dirname + '/../views/weatherResults.hbs')});
-                res.render('weatherResults', render_dict);
-            });
-        });
     }
 );
 };
