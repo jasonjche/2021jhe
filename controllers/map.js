a363c55d691795dbfafa3edb89b949b9a9bf49a8
Jason He <jasonhe.md@gmail.com>
2020-04-19 22:10:30 -0400

    Co-authored-by: Alex L <alexlin247@gmail.com>

--- controllers/map.js
+++ controllers/map.js
@@ -2,7 +2,7 @@ var https = require('https');
 var fs = require('fs');
 
 module.exports.run_setup = function(app) {
-app.get('/map', (req, res) => {
+app.get('/corona', (req, res) => {
         res.render('partials/template', {name : 'Map Game', content : fs.readFileSync(__dirname + '/../views/map.hbs')});
 });
 };
